15e093176e19b078c5ed19651668eddaddce2a30
lniv <nivl2000@gmail.com>
2019-06-23 23:36:52 -0700

    ogn2gp.py - fixing line ends form windows heritage, and some import issues due to org changes.

--- x2gpaero/ogn2gp.py
+++ x2gpaero/ogn2gp.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
 """
 handle ogn to gpaero; it's really aprs as well, so it makes some sense 
 for it to live in the same rpository.
 i'm splitting the file for now mostly for convenience; logically it's a bit murky.
 """
 
-from aprs2gpaero import APRSIS2GPRAW, config_file_reader, _USABLE_KEYWORDS
+import argparse
+from x2gpaero.aprs2gp import APRSIS2GPRAW, config_file_reader, _USABLE_KEYWORDS
 from ogn.parser import parse as ogn_parse
 from ogn.parser import ParseError as OGNParseError
 from ogn.client import settings as ogn_settings
